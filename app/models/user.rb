c13ca97b15f48e47ceb2f4a91bf2916e0f130c46
NattuSakurai <71743081+NattuSakurai@users.noreply.github.com>
2020-10-22 11:42:44 +0900

    細かいバリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 
-  validates :nickname, presence: true
+  validates :nickname, presence: true, length: { maximum: 40 }
   validates :birthday, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+  end
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, } do
+    validates :first_name_kana, presence: true
+    validates :last_name_kana, presence: true
+  end
 end
