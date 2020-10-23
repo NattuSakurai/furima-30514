9b52cfbbe23d9508a4735c2179ee2cd0c6053f23
NattuSakurai <71743081+NattuSakurai@users.noreply.github.com>
2020-10-23 19:26:42 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,10 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :detail
+
+  validates :name, :description, :category_id, :status_id, :postage_id, :regional_id, :shipping_date_id, :price, presence: true
+
+  validates :category_id, :status_id, :postage_id, :regional_id, :shipping_date_id, numericality: { other_than: 1 } 
 end
