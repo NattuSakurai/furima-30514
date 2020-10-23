ac5e2ebd570b01ad6e0fadcbb100174660df920d
NattuSakurai <71743081+NattuSakurai@users.noreply.github.com>
2020-10-23 12:46:00 +0900

    FactoryBot内の記述変更

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials(number: 5)}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {Faker::Alphanumeric.alphanumeric(number: 6, min_alpha: 1, min_numeric: 1)}
     password_confirmation {password}
-    first_name            {Faker::Japanese::Name.first_name}
-    last_name             {Faker::Japanese::Name.last_name}
-    first_name_kana       {Faker::Japanese::Name.first_name.yomi}
-    last_name_kana        {Faker::Japanese::Name.last_name.yomi}
+    first_name            {"山田"}
+    last_name             {"陸太郎"}
+    first_name_kana       {"ヤマダ"}
+    last_name_kana        {"リクタロウ"}
     birthday              {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
   end
 end
