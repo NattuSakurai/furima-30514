5f1f359d90fd84a8e5aa0d847a4c8cc5e533f013
NattuSakurai <71743081+NattuSakurai@users.noreply.github.com>
2020-10-24 14:55:50 +0900

    発送元までの日数のプルダウン作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,10 +6,12 @@ class Item < ApplicationRecord
   belongs_to_active_hash :status
   belongs_to_active_hash :postage
   belongs_to_active_hash :regional
+  belongs_to_active_hash :shipping_date
 
   validates :name, :description, :category_id, :status_id, :postage_id, :regional_id, :shipping_date_id, :price, presence: true
 
   validates :category_id, numericality: { other_than: 1 } 
   validates :status_id, numericality: { other_than: 1 } 
   validates :regional_id, numericality: { other_than: 1 } 
+  validates :shipping_date_id, numericality: { other_than: 1 } 
 end
