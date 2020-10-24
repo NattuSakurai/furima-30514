b19dbf51686a8ad3256897dfd30f327e96959213
NattuSakurai <71743081+NattuSakurai@users.noreply.github.com>
2020-10-24 13:06:27 +0900

    createアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,22 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    redirect_to root_path
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :postage_id, :regional_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
   end
 end
