8398452cb13427f9f4273f15aee99cef9dbfe344
NattuSakurai <71743081+NattuSakurai@users.noreply.github.com>
2020-10-24 13:57:49 +0900

    カテゴリーのプルダウン作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,9 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :detail
+  belongs_to_active_hash :category
 
   validates :name, :description, :category_id, :status_id, :postage_id, :regional_id, :shipping_date_id, :price, presence: true
 
-  validates :category_id, :status_id, :postage_id, :regional_id, :shipping_date_id, numericality: { other_than: 1 } 
+  validates :category_id, numericality: { other_than: 1 } 
 end
--- spec/factories/statuses.rb
+++ spec/factories/statuses.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :detail do
+  factory :status do
     
   end
 end
