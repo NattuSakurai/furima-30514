969ecd73eae70a21c86f37eb731eb94667c0146c
NattuSakurai <71743081+NattuSakurai@users.noreply.github.com>
2020-10-25 00:33:06 +0900

    商品出品の単体テストコードの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,6 +12,7 @@ class Item < ApplicationRecord
 
   validates :category_id, numericality: { other_than: 1 } 
   validates :status_id, numericality: { other_than: 1 } 
+  validates :postage_id, numericality: { other_than: 1 } 
   validates :regional_id, numericality: { other_than: 1 } 
   validates :shipping_date_id, numericality: { other_than: 1 } 
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
-
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  describe '商品出品' do
+    context '商品が出品できる場合' do
+      it "image,name,description,category_id,status_id,postage_id,regional_id,shipping_date_id,priceが存在すれば出品できる" do
+      expect(@item).to be_valid
+      end
+    end
+    context '商品が出品できない場合' do
+      it "imageが空だと登録できない" do
+      end
+      it "nameが空だと登録できない" do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it "descriptionが空だと登録できない" do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Description can't be blank")
+      end
+      it "category_idが空だと登録できない" do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it "status_idが空だと登録できない" do
+        @item.status_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+      it "postage_idが空だと登録できない" do
+        @item.postage_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage can't be blank")
+      end
+      it "regional_idが空だと登録できない" do
+        @item.regional_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Regional can't be blank")
+      end
+      it "shipping_date_idが空だと登録できない" do
+        @item.shipping_date_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping date can't be blank")
+      end
+      it "priceが空だと登録できない" do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+    end
+  end
 end
