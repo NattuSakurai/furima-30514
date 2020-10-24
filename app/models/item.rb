a5522014e2a43792d8fe3767e7bfdbf41cec9fa0
NattuSakurai <71743081+NattuSakurai@users.noreply.github.com>
2020-10-24 14:08:03 +0900

    商品の状態のプルダウン作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,8 +3,10 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
-
+  belongs_to_active_hash :status
+  
   validates :name, :description, :category_id, :status_id, :postage_id, :regional_id, :shipping_date_id, :price, presence: true
 
   validates :category_id, numericality: { other_than: 1 } 
+  validates :status_id, numericality: { other_than: 1 } 
 end
