ffb34647d61cf790fff63d8028c9b167d2324ad7
NattuSakurai <71743081+NattuSakurai@users.noreply.github.com>
2020-10-26 11:38:12 +0900

    itemsのFactoryBot内の記述変更
    文字列を数値に変更しました

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,11 +3,11 @@ FactoryBot.define do
     image               {"apple.png"}
     name                {"りんご"}
     description         {"青森産のりんごです"}
-    category_id            {"10"}
-    status_id              {"2"}
-    postage_id             {"2"}
-    regional_id            {"3"}
-    shipping_date_id       {"3"}
-    price               {"1980"}
+    category_id            {10}
+    status_id              {2}
+    postage_id             {2}
+    regional_id            {3}
+    shipping_date_id       {3}
+    price               {1980}
   end
 end
