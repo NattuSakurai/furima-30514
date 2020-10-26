7a71cf8cd278759e7b27fc38ab00817e9386ccee
NattuSakurai <71743081+NattuSakurai@users.noreply.github.com>
2020-10-26 18:20:09 +0900

    商品一覧表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+  has_many :items
+  has_many :records
+
   EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates_format_of :email, with: EMAIL_REGEX
 
