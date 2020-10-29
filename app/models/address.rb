bfe7cdf8311619c7bc338cd39693ed443d0c3bf6
NattuSakurai <71743081+NattuSakurai@users.noreply.github.com>
2020-10-29 15:34:49 +0900

    addressのバリデーション(仮)

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,11 @@
 class Address < ApplicationRecord
   belongs_to :record
   
-  validates :postal_code, presence: true
-  validates :prefectures_id, presence: true
-  validates :city, presence: true
-  validates :address_number, presence: true
-  validates :phone_number, presence: true
-  validates :record, presence: true
+  with_options presence: true do
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/}
+    validates :prefectures_id, numericality: { other_than: 1 }
+    validates :city, format: { with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :address_number
+    validates :phone_number
+  end
 end
