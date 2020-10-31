99ad0643a959f43f7ee11acd2c34fe20cdd2d458
NattuSakurai <71743081+NattuSakurai@users.noreply.github.com>
2020-10-31 13:59:36 +0900

    複数のテーブルに情報を保存する実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,8 +1,18 @@
 class RecordsController < ApplicationController
   def index
-    @form = UserShopping.all
+    @user_shopping = UserShopping.new
   end
   def create
-    
+    @user_shopping = UserShopping.new(shopping_params)
+    if @user_shopping.valid?
+      @user_shopping.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+  private
+  def shopping_params
+    params.permit(:postal_code, :prefectures_id, :city, :address_number, :building_name, :phone_number, :record_id).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
